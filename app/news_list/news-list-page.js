2470fa16995243d689dac3ea8605f35d9859736e
Andrea Stagi <stagi.andrea@gmail.com>
2018-10-27 14:41:26 +0200

    Add lottie animation for side drawer menu button

--- app/news_list/news-list-page.js
+++ app/news_list/news-list-page.js
@@ -9,16 +9,21 @@ const topmost = require("tns-core-modules/ui/frame").topmost;
 
 const listViewModule = require("tns-core-modules/ui/list-view");
 
+const LottieView = require('nativescript-lottie');
+
 const AnsaService = require("../shared/ansaservice");
 
 var view = require("ui/core/view");
 var drawer;
-
+var drawerButton;
+var drawerButtonItem;
+var drawerOpen = false;
 
 function onNavigatingTo(args) {
     const page = args.object;
     page.bindingContext = new HomeViewModel();
     drawer = view.getViewById(page, "sideDrawer");
+    drawerButtonItem = view.getViewById(page, "sideButtonDrawer");
     AnsaService.listNews().then((r) => {
         console.log(r);
         page.bindingContext.news = r;
@@ -51,6 +56,36 @@ function onItemTap(args) {
 
 exports.onItemTap = onItemTap;
 
+
 exports.toggleDrawer = function() {
     drawer.toggleDrawerState();
+};
+
+exports.drawerButtonLoaded = function(args) {
+    drawerButton = args.object;
+    drawerButton.speed = 0.1;
+};
+
+exports.onTapDrawer = function(args) {
+    drawerOpen = !drawerOpen;
+    console.log(drawerButton._animationView.animationSpeed);
+    drawerButton._animationView.animationSpeed = 2.5;
+    if (drawerOpen == true) {
+        drawerButton._animationView.playFromProgressToProgressWithCompletion(1.0, 0.5, null);
+    } else {
+        drawerButton._animationView.playFromProgressToProgressWithCompletion(0.5, 1.0, null);
+    }
+
+};
+
+exports.onDrawerClosed = function(args) {
+    drawerOpen = false;
+    drawerButton.cancelAnimation();
+    drawerButton._animationView.animationProgress = 1.0;
+};
+
+exports.onDrawerOpened = function(args) {
+    drawerOpen = true;
+    drawerButton.cancelAnimation();
+    drawerButton._animationView.animationProgress = 0.5;
 };
